394f881d91ca882c1759f298a58f0f6a053699e0
Digital 11 <digital11@Digitals-MacBook-Pro-11.local>
2014-11-12 09:16:36 -0500

    Initial Commit

---
+++ CPD2164-BuildIt4-Xcode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CPD2164-BuildIt4-Xcode
+//
+//  Created by Digital 11 on 11/12/2014.
+//  Copyright (c) 2014 Len Payne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
